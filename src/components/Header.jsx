d1036261508a1f133c4eef47f60adcbc2bc15898
AvanikaGyanaji <avanika.gyanaji@anvi.co>
2025-11-11 15:55:51 +0530

    Update Survillance page (animation)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { Survillance } from "../pages/Survillance";
 
 export const Header=()=>{
     return(<header className="header fixed top-[56px] w-full flex justify-end items-center px-[90px] z-[100]">
@@ -19,7 +20,7 @@ export const Header=()=>{
       {/* Navigation */}
       <nav className="nav flex gap-x-[22px] text-lg">
        <Link onClick={() => document.getElementById("about")?.scrollIntoView({ behavior: "smooth" })} className="navlink">About</Link>
-       <Link onClick={() => document.getElementById("about")?.scrollIntoView({ behavior: "smooth" })} className>Products</Link>
+       <Link to="/survillance" className>Products</Link>
        <Link onClick={() => document.getElementById("about")?.scrollIntoView({ behavior: "smooth" })} className>Technologies</Link>
        <Link onClick={() => document.getElementById("about")?.scrollIntoView({ behavior: "smooth" })} className>Contact</Link>
 
